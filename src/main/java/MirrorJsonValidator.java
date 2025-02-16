4f8aca8279f4bc9e8d247488ac1c64578029bcff
aidanhhhowarth <howartah@clarkson.edu>
2025-02-16 16:29:12 -0500

    fixed colors schema and moved things around

--- src/main/java/MirrorJsonValidator.java
+++ src/main/java/MirrorJsonValidator.java
@@ -5,39 +5,33 @@ import com.networknt.schema.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.util.Set;
 
 public class MirrorJsonValidator {
 
-	public static final URI SCHEMA_URI = new File("mirrors.schema.json").getAbsoluteFile().toURI(); // location of mirrors.schema.json
+	private static final JsonSchema SCHEMA = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7)
+			.getSchema(new File("mirrors.schema.json").getAbsoluteFile().toURI());
 
 	/**
 	 * Validates mirrors.json against mirrors.schema.json.
 	 *
-	 * @param  mirrorsJson  the mirrors.json file to be validated
-	 * @return      true if mirrorsJson is a valid json matching schema, false otherwise
+	 * @param mirrorsJson the mirrors.json file to be validated
+	 * @return true if mirrorsJson is a valid json matching schema, false otherwise
 	 */
 	public static boolean ValidateMirrorJson(File mirrorsJson) {
-
-		JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
-
-		JsonSchema jsonSchema = jsonSchemaFactory.getSchema(SCHEMA_URI); // it is apparently very important that this URI is absolute
 
 		JsonNode mirrorsJsonNode;
 		try {
 			mirrorsJsonNode = new JsonMapper().readTree(mirrorsJson);
-		} catch (JsonParseException e) { // Invalid json
+		} catch (JsonParseException e) {
 			System.out.println("mirrors.json contains invalid json");
-			e.printStackTrace();
 			return false;
 		} catch (IOException e) {
 			System.out.println("Failed to read mirrors.json from file");
 			return false;
 		}
 
-		Set<ValidationMessage> errors = jsonSchema.validate(mirrorsJsonNode);
-
+		Set<ValidationMessage> errors = SCHEMA.validate(mirrorsJsonNode);
 		if (errors.isEmpty()) {
 			System.out.println("mirrors.json is valid!");
 			return true;
